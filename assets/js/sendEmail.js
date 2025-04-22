2727dab29123b5184d3ed88c2e8824011f722db7
David O'Quigley <oquigld2@gmail.com>
2025-04-22 10:51:52 +0000

    Add emailjs integration on contact.html

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,19 @@
+function sendMail(contactForm) {
+    
+    var templateParams = {
+        'from_name': contactForm.name.value,
+        'from_email': contactForm.emailaddress.value,
+        'project_request': contactForm.projectsummary.value,
+    };
+
+    emailjs.send("service_jiubjee", "Project_Rosie", templateParams)
+        .then(
+            function (response) {
+                console.log('Success', response);
+            },
+            function (error) {
+                console.log('Failed', error);
+            });
+    return false;
+}
+
